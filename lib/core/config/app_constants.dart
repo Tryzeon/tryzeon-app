78ea79ab5d47eab2a523ddfaf3eedff73ac27501
erichung9060 <rthung96@gmail.com>
2026-01-31 18:43:21 +0800

    chore: update default profile image asset path constant to `default.png`

--- lib/core/config/app_constants.dart
+++ lib/core/config/app_constants.dart
@@ -22,7 +22,7 @@ class AppConstants {
   static const String functionTryon = 'tryon';
 
   // Assets
-  static const String defaultProfileImage = 'assets/images/profile/default_profile.png';
+  static const String defaultProfileImage = 'assets/images/profile/default.png';
   static const String tryOnSkeleton = 'assets/images/tryon/skeleton.svg';
 
   // Timeouts & Durations
