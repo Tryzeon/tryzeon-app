0912ded8728b4bb4e1507612b9331a8cac59abc0
erichung9060 <rthung96@gmail.com>
2026-03-16 03:17:51 +0800

    feat: add VideoPromptConfigNotifier Riverpod provider

--- lib/feature/personal/settings/providers/settings_providers.dart
+++ lib/feature/personal/settings/providers/settings_providers.dart
@@ -1,5 +1,6 @@
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 import 'package:tryzeon/core/di/core_providers.dart';
+import 'package:tryzeon/feature/personal/home/domain/entities/video_prompt_config.dart';
 import 'package:tryzeon/feature/personal/settings/data/repositories/settings_repository_impl.dart';
 import 'package:tryzeon/feature/personal/settings/domain/repositories/settings_repository.dart';
 import 'package:typed_result/typed_result.dart';
@@ -48,5 +49,25 @@ class RecommendNearbyShops extends _$RecommendNearbyShops {
       // Optionally set state to error or just revert
       // For now, we update state only on success
     }
+  }
+}
+
+@riverpod
+class VideoPromptConfigNotifier extends _$VideoPromptConfigNotifier {
+  @override
+  Future<VideoPromptConfig> build() async {
+    final repository = ref.read(settingsRepositoryProvider);
+    final result = await repository.getVideoPromptConfig();
+    return result.isSuccess ? result.get()! : const VideoPromptConfig();
+  }
+
+  Future<bool> save(final VideoPromptConfig config) async {
+    final repository = ref.read(settingsRepositoryProvider);
+    final result = await repository.setVideoPromptConfig(config);
+    if (result.isSuccess) {
+      state = AsyncData(config);
+      return true;
+    }
+    return false;
   }
 }
