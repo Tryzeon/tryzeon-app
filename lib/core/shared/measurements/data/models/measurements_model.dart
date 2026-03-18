b60e74f543d4e2dd7f572b110015da14666ca52a
erichung9060 <rthung96@gmail.com>
2026-03-18 16:47:39 +0800

    refactor(measurements): exclude null fields from JSON serialization

--- lib/core/shared/measurements/data/models/measurements_model.dart
+++ lib/core/shared/measurements/data/models/measurements_model.dart
@@ -2,7 +2,7 @@ import 'package:json_annotation/json_annotation.dart';
 
 part 'measurements_model.g.dart';
 
-@JsonSerializable(fieldRename: FieldRename.snake)
+@JsonSerializable(fieldRename: FieldRename.snake, includeIfNull: false)
 class MeasurementsModel {
   const MeasurementsModel({
     this.height,
