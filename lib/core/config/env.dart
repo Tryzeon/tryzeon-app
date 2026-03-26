8f32fc5e3899eb5451652787feedd0b3819092f5
erichung9060 <rthung96@gmail.com>
2026-03-26 13:52:45 +0800

    chore(config): add RevenueCat configuration and API key

--- lib/core/config/env.dart
+++ lib/core/config/env.dart
@@ -9,4 +9,7 @@ abstract class Env {
 
   @EnviedField(varName: 'SUPABASE_ANON_KEY', defaultValue: 'placeholder-key')
   static final String supabaseAnonKey = _Env.supabaseAnonKey;
+
+  @EnviedField(varName: 'REVENUE_CAT_API_KEY', defaultValue: 'placeholder-key')
+  static final String revenueCatApiKey = _Env.revenueCatApiKey;
 }
